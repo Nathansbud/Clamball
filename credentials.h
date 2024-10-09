a7a15b8c153a3d07d30d1e7216397254ce86195d
Zack Amiton <zamiton@outlook.com>
2024-10-09 12:03:18 -0400

    add comment

--- credentials.h
+++ credentials.h
@@ -1,4 +1,6 @@
-// Credentials header file used for network development; 
-// DO NOT COMMIT THIS WITH SECRETS!
+// SSID/password should be filled in here for the relevant network;
+// this should not be committed with secrets; run:
+// git update-index --skip-worktree credentials.h
+// to capture this behavior while allowing location modification :)
 #define SECRET_SSID "REPLACE_ME"
 #define SECRET_PASS "REPLACE_ME"
